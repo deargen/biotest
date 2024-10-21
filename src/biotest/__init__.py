8531d0534f7a67193f6b5297006dcbbeb911d690
Kiyoon Kim <kiyoon@users.noreply.github.com>
2024-10-21 17:42:36 +0900

    refactor: remove log utils

--- src/biotest/__init__.py
+++ src/biotest/__init__.py
@@ -1,21 +1,3 @@
-import os
-from pathlib import Path
-
 from . import _version
 
 __version__ = _version.get_versions()["version"]
-
-default_log_level = os.environ.get("BIOTEST_LOG_LEVEL")
-if default_log_level is None:
-    default_log_level = "INFO"
-
-# NOTE: It is assuming that you installed with `pip install -e .`
-#      If you installed with `pip install .`, then the path will be different.
-PROJECT_DIR = Path(__file__).parent.parent.parent
-
-DATA_DIR = os.environ.get("BIOTEST_DATA_DIR")
-DATA_DIR = (
-    PROJECT_DIR / "data" if DATA_DIR is None or DATA_DIR == "" else Path(DATA_DIR)
-)
-
-LOG_DIR = DATA_DIR / "logs"
--- src/biotest/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from biotest.utils.log import setup_logging
-
-__all__ = [
-    "setup_logging",
-]
